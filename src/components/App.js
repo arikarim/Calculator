67a7bc609ffa8f5e1e89618f87472768a37d3eef
arikarim <arikarim146@gmail.com>
2021-05-14 21:39:07 +0300

    Add display component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Display from './Display';
+
+const App = () => (
+  <div>
+    <Display />
+  </div>
+);
+
+App.propTypes = {
+
+};
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = (props) => {
+  return <div>ok</div>
+}
+
+
+Display.propTypes = {
+
+};
+
+export default Display;
