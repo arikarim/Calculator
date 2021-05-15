3f581a1fe41b3e77bb34f409b9f467d2746a4905
arikarim <arikarim146@gmail.com>
2021-05-15 21:54:56 +0300

    Import calculate.js to App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import '../App.css';
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/Calculate';
 
 const App = () => (
   <div className="col-2 mx-auto">
