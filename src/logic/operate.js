ea0ccfa9e1e8b14e73fcee2a64350c4a343115a6
arikarim <arikarim146@gmail.com>
2021-05-15 21:31:34 +0300

    Add operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,4 +1,9 @@
+import Big from 'big.js';
 
-const calculate = (calculateData, buttonName) => {
-
+ const operate = (numberOne, numberTwo, operation) => {
+  num1 = new Big(numberOne);
+  num2 = new Big(numberTwo)
+  return `${num1} ${operation} ${num2}` 
 }
+
+export default operate
