b3c1413061311e1f08ce38f301441a34909705ab
arikarim <arikarim146@gmail.com>
2021-05-16 15:41:55 +0300

    Add statefull app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
 import '../App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/Calculate';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       total: null,
       next: null,
       operation: null,
-    }
-    this.handleClick = this.handleClick.bind(this)
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  // function handleClick() {
-  //   return Calculate;
-  // }
+  handleClick() {
+    return Calculate;
+  }
+
   render() {
     return (
       <div className="col-2 mx-auto">
         <Display />
         <ButtonPanel onClick={handleClick} />
-    </div>
-    )
+      </div>
+    );
   }
 }
 
