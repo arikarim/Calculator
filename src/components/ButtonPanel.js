a68920bfc8d743538daa0ee0ebd18559897980a5
arikarim <arikarim146@gmail.com>
2021-05-16 16:33:19 +0300

    Add clickHandler to button and buttonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,44 +1,44 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => (
+const ButtonPanel = (props) => (
   <div>
     <div className="d-flex">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="/" />
+      <Button name="AC" handleClick={() => props.clickHandler('0')} />
+      <Button name="+/-" handleClick={() => props.clickHandler('0')} />
+      <Button name="%" handleClick={() => props.clickHandler('0')} />
+      <Button name="/" handleClick={() => props.clickHandler('0')} />
     </div>
     <div className="d-flex">
-      <Button name={7} />
-      <Button name={8} />
-      <Button name={9} />
-      <Button name="X" />
+      <Button name={7} handleClick={() => props.clickHandler('0')} />
+      <Button name={8} handleClick={() => props.clickHandler('0')} />
+      <Button name={9} handleClick={() => props.clickHandler('0')} />
+      <Button name="X" handleClick={() => props.clickHandler('0')} />
     </div>
     <div className="d-flex">
-      <Button name={4} />
-      <Button name={5} />
-      <Button name={6} />
-      <Button name="-" />
+      <Button name={4} handleClick={() => props.clickHandler('0')} />
+      <Button name={5} handleClick={() => props.clickHandler('0')} />
+      <Button name={6} handleClick={() => props.clickHandler('0')} />
+      <Button name="-" handleClick={() => props.clickHandler('0')} />
     </div>
     <div className="d-flex">
-      <Button name={1} />
-      <Button name={2} />
-      <Button name={3} />
-      <Button name="+" />
+      <Button name={1} handleClick={() => props.clickHandler('0')} />
+      <Button name={2} handleClick={() => props.clickHandler('0')} />
+      <Button name={3} handleClick={() => props.clickHandler('0')} />
+      <Button name="+" handleClick={() => props.clickHandler('0')} />
     </div>
     <div className="d-flex">
-      <Button name={0} />
-      <Button name="." />
-      <Button name="=" />
-      <Button name="GO" />
+      <Button name="0" handleClick={() => props.clickHandler('0')} />
+      <Button name="." handleClick={() => props.clickHandler('0')} />
+      <Button name="=" handleClick={() => props.clickHandler('0')} />
+      <Button name="GO" handleClick={() => props.clickHandler('0')} />
     </div>
   </div>
 );
 
 ButtonPanel.propTypes = {
-
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default ButtonPanel;
