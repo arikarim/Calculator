7ea93a5a998a0fbf551c7bbac4f1ed474a80a144
arikarim <arikarim146@gmail.com>
2021-05-16 16:38:59 +0300

    Finish

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,34 +5,34 @@ import Button from './Button';
 const ButtonPanel = (props) => (
   <div>
     <div className="d-flex">
-      <Button name="AC" handleClick={() => props.clickHandler('0')} />
-      <Button name="+/-" handleClick={() => props.clickHandler('0')} />
-      <Button name="%" handleClick={() => props.clickHandler('0')} />
-      <Button name="/" handleClick={() => props.clickHandler('0')} />
+      <Button name="AC" handleClick={() => props.clickHandler('AC')} />
+      <Button name="+/-" handleClick={() => props.clickHandler('+/-')} />
+      <Button name="%" handleClick={() => props.clickHandler('%')} />
+      <Button name="/" handleClick={() => props.clickHandler('/')} />
     </div>
     <div className="d-flex">
-      <Button name={7} handleClick={() => props.clickHandler('0')} />
-      <Button name={8} handleClick={() => props.clickHandler('0')} />
-      <Button name={9} handleClick={() => props.clickHandler('0')} />
-      <Button name="X" handleClick={() => props.clickHandler('0')} />
+      <Button name={7} handleClick={() => props.clickHandler('7')} />
+      <Button name={8} handleClick={() => props.clickHandler('8')} />
+      <Button name={9} handleClick={() => props.clickHandler('9')} />
+      <Button name="X" handleClick={() => props.clickHandler('X')} />
     </div>
     <div className="d-flex">
-      <Button name={4} handleClick={() => props.clickHandler('0')} />
-      <Button name={5} handleClick={() => props.clickHandler('0')} />
-      <Button name={6} handleClick={() => props.clickHandler('0')} />
-      <Button name="-" handleClick={() => props.clickHandler('0')} />
+      <Button name={4} handleClick={() => props.clickHandler('4')} />
+      <Button name={5} handleClick={() => props.clickHandler('5')} />
+      <Button name={6} handleClick={() => props.clickHandler('6')} />
+      <Button name="-" handleClick={() => props.clickHandler('-')} />
     </div>
     <div className="d-flex">
-      <Button name={1} handleClick={() => props.clickHandler('0')} />
-      <Button name={2} handleClick={() => props.clickHandler('0')} />
-      <Button name={3} handleClick={() => props.clickHandler('0')} />
-      <Button name="+" handleClick={() => props.clickHandler('0')} />
+      <Button name={1} handleClick={() => props.clickHandler('1')} />
+      <Button name={2} handleClick={() => props.clickHandler('2')} />
+      <Button name={3} handleClick={() => props.clickHandler('3')} />
+      <Button name="+" handleClick={() => props.clickHandler('+')} />
     </div>
     <div className="d-flex">
       <Button name="0" handleClick={() => props.clickHandler('0')} />
-      <Button name="." handleClick={() => props.clickHandler('0')} />
-      <Button name="=" handleClick={() => props.clickHandler('0')} />
-      <Button name="GO" handleClick={() => props.clickHandler('0')} />
+      <Button name="." handleClick={() => props.clickHandler('.')} />
+      <Button name="=" handleClick={() => props.clickHandler('=')} />
+      <Button name="GO" handleClick={() => props.clickHandler('')} />
     </div>
   </div>
 );
