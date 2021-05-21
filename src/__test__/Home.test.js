b6c9b05713d71ffa459252628f3108e9eea74d57
arikarim <arikarim146@gmail.com>
2021-05-21 11:16:16 +0300

    Add Home tests

---
+++ src/__test__/Home.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import NavBar from '../components/Nav';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <>
+        <BrowserRouter>
+          <NavBar />
+        </BrowserRouter>
+        <h1>Math Magicians</h1>
+        <h6 className="title">Welcome to our page!</h6>
+      </>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
